b718b7a24d1253aa85210e90edee9d9ba6ca8b80
Aidit-Models <91160383+Aidit-Models@users.noreply.github.com>
2023-06-05 16:33:44 +0800

    Update predictor.py

--- predictors/ON-target/predictor.py
+++ predictors/ON-target/predictor.py
@@ -7,11 +7,11 @@ import os
 
 
 
-# 检查输入是否为路径
+# check path 
 def is_valid_path(path):
     return os.path.isfile(path) or os.path.isdir(path)
 
-# 生成路径文件
+# create folder 
 def mkdir(path):
     path = path.strip()
     path = path.rstrip("\\")
@@ -19,35 +19,35 @@ def mkdir(path):
     if not isExists:
         os.makedirs(path)
 
-# 定义预测函数
+# main function
 def pred():
     parser = argparse.ArgumentParser()
-    ## 输入细胞系
+    ## cell line
     parser.add_argument(
         "--cellline",
         type=str,
         help="choose K562 cellline or Jurakt cellline ",
     )
-    ## 输入文件路径或者序列信息
+    ## input file or input sequence 
     parser.add_argument(
         "--input",
         type=str,
         help="path of data for training and testing or just sequence",
     )
-    ## 输入想要选择的模型
+    ## choose model
     parser.add_argument(
         "--model",
         type=str,
         help="choose the model you would like to use (linear, xgb,lgb, rnn,tcn)",
     )
-    ## 输入模型的保存路进
+    ## on-target model library
     parser.add_argument(
         "--model_library",
         type=str,
         default='.',
         help="model_library path",
     )
-    ## 输出结果路径
+    ## save path
     parser.add_argument(
         "--output_dir",
         type=str,
@@ -56,21 +56,21 @@ def pred():
     )
     args = parser.parse_args()
     
-    ##检查输入是否为路径或者单纯的字符号
+    ##check input type
     if is_valid_path(args.input):
         data = find_all(args.input,file=True)
     else:
         data = find_all(args.input,file=False)
 
-    ##检查输出路径，如果不存在并创建
+    ##creat folder 
     mkdir(args.output_dir)
-    ## 对数据进行处理并做成我们想要的输出
+    ## prepare data
     data = pd.DataFrame(data)
     data.columns = ['sequence']
     one_hot_1d_input = creat_data_for_1d_input('one_hot',data)
     embedding_1d_input = creat_data_for_1d_input('embedding',data)
 
-    ## 选择我们想要的模型进行预测
+    ## choose model
     if 'ridge' in args.model or 'xgb' in args.model or 'linear' in args.model or "lgb" in args.model:
         model_selection = f'{args.model_library}/{args.cellline}_best_model/{args.model}_best.sav'
         ml = joblib.load(model_selection)
